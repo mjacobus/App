ea1139f30d842fe5c0132c020309317248b73e94
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2010-12-02 21:09:58 -0200

    Pequena alteracao no modulo abstract. Adicionado App_Crud e App_Loader

--- Model/Abstract.php
+++ Model/Abstract.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Base model for the application model
  *
@@ -10,66 +11,126 @@ abstract class App_Model_Abstract
     /**
      * @var array messages for communicationt to the outside world
      */
-     protected $_messages = array();
+    protected $_messages = array(
+        'info' => array(),
+        'error' => array(),
+        'warning' => array()
+    );
 
-     /**
-      * Sets a message
-      * @param string $value message
-      * @param string $key associated key for a message
-      * @return App_Model_Abstract
-      */
-     public function setMessage($value, $key = null)
-     {
-        if ($key !== null) {
-            $this->_messages[$key] = $message;
-        } else {
-            $this->_messages[] = $message;
-        }
+    /**
+     * Add a message
+     *
+     * @param string $value message
+     * @param string $type
+     * @return App_Model_Abstract
+     */
+    public function addMessage($message, $type = 'info')
+    {
+        $this->_messages[$type][] = (string) $message;
         return $this;
-     }
+    }
 
-     /**
-      * Sets lots of messages
-      * @param array $messages
-      * @return App_Model_Abstract
-      */
-     public function setMessages(array $messages = array()) {
-        foreach($messages as $key => $message) {
-            if (is_string($key)) 
-                $this->setMessage($message, $key);
-            else
-                $this->setMessage($message);
-        }
-        return $this;
-     }
-     
-     /**
-      * Add a message
-      *
-      * @param string $value message
-      * @param string $key associated key for a message
-      * @param bool $force whether should overwrite existing key
-      * @return App_Model_Abstract
-      */
-     public function addMessage($message, $key = null, $force = false)
-     {
-         if ($key == null)
-             $this->_messages[] = $message;
-         else {
-            if (($force) && (!array_key_exists($key, $this->getMessages()))) {
-                throw new App_Model_Exception('Key ' . $key . ' already exists.');
+    /**
+     * Add message of type 'info'
+     * @param string $message
+     * @return App_Model_Abstract
+     */
+    public function addInfoMessage($message)
+    {
+        return $this->addMessage($message);
+    }
+
+    /**
+     * Get messages of type 'info'
+     * @return array
+     */
+    public function getInfoMessages()
+    {
+        return $this->getMessages('info');
+    }
+
+    /**
+     * Add message of type 'error'
+     * @param string $message
+     * @return App_Model_Abstract
+     */
+    public function addErrorMessage($message)
+    {
+        return $this->addMessage($message, 'error');
+    }
+
+    /**
+     * Get messages of type 'error'
+     * @return array
+     */
+    public function getErrorMessages()
+    {
+        return $this->getMessages('error');
+    }
+
+    /**
+     * Add message of type 'warning'
+     * @param string $message
+     * @return App_Model_Abstract
+     */
+    public function addWarningMessage($message)
+    {
+        return $this->addMessage($message, 'warning');
+    }
+
+    /**
+     * Get messages of type 'warning'
+     * @return array
+     */
+    public function getWarningMessages()
+    {
+        return $this->getMessages('warning');
+    }
+
+    /**
+     *
+     * @param string $type
+     * @return App_Model_Abstract
+     */
+    public function resetMessages($type = null)
+    {
+        if ($type == null) {
+            foreach ($this->_messages as $type => $messages) {
+                $this->_messages[$type] = array();
             }
-            $thsi->_messages[$key] = $message;
-         }
-         return $this;
-     }
+        } else {
+            $this->_messages[$type] = array();
+        }
+        return $this;
+    }
 
-     /**
-      * Gets messages
-      * @return array
-      */
-     public function getMessages(){
+    /**
+     * Get all messages of a given type,.
+     * @param string $type defaults to info
+     * @return array
+     */
+    public function getMessages($type = 'info')
+    {
+        return $this->_messages[$type];
+    }
+
+    /**
+     * Get messages of all the types
+     * @return array
+     */
+    public function getAllMessages()
+    {
         return $this->_messages;
-     }
+    }
+
+    /**
+     * Get base uri
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        $fc = Zend_Controller_Front::getInstance();
+        return $fc->getBaseUrl();
+    }
 
 }
